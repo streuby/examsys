bacab91f5b74f5d868983559dd5c518c78d97255
Stephen Amade Reuben <streuby@live.com>
2021-12-27 22:52:47 +0100

    still testing error handling

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,9 +16,9 @@ def login():
 		if form.validate_on_submit():
 			user = User.query.filter_by(username=form.username.data).first()
 
-			if user is not None and user.verify_password(form.password.data):
-				login_user(user, form.remember_me.data)
-				return redirect(request.args.get('next') or url_for('main.index', known = True))
+			# if user is not None and user.verify_password(form.password.data):
+			# 	login_user(user, form.remember_me.data)
+			# 	return redirect(request.args.get('next') or url_for('main.index', known = True))
 
 			flash(u'Invalid username or password')
 	except Exception as e:
