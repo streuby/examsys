ac376e0d951b4335f4d6cfd44ed7ca5d0c438628
Stephen Amade Reuben <streuby@live.com>
2021-12-28 10:09:48 +0100

    explit configuration

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from app.models import User, Role
 from flask_migrate import Migrate
 
 #app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-app = create_app('development')
+app = create_app('production')
 #manager = Manager(app)
 migrate = Migrate(app, db)
 
