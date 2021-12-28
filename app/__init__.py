7870a5079ed7bea388e59a3e107ba8d686132cfd
Stephen Amade Reuben <streuby@live.com>
2021-12-28 06:24:16 +0100

    configure logging handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from config import config
 
@@ -49,9 +51,40 @@ def create_app(config_name):
     #     app.logger.setLevel(logging.DEBUG)
     #     app.logger.debug('this will show in the log')
     
-    if __name__ != '__main__':
-        gunicorn_logger = logging.getLogger('gunicorn.error')
-        app.logger.handlers = gunicorn_logger.handlers
-        app.logger.setLevel(gunicorn_logger.level)
+    # if __name__ != '__main__':
+    #     gunicorn_logger = logging.getLogger('gunicorn.error')
+    #     app.logger.handlers = gunicorn_logger.handlers
+    #     app.logger.setLevel(gunicorn_logger.level)
+    
+    # Configure customer logger
+    if not app.debug and not app.testing:
+        # if app.config['MAIL_SERVER']:
+        #     auth = None
+        #     if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+        #         auth = (app.config['MAIL_USERNAME'],
+        #                 app.config['MAIL_PASSWORD'])
+        #     secure = None
+        #     if app.config['MAIL_USE_TLS']:
+        #         secure = ()
+        #     mail_handler = SMTPHandler(
+        #         mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+        #         fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+        #         toaddrs=app.config['ADMINS'], subject='Fliptz Failure',
+        #         credentials=auth, secure=secure)
+        #     mail_handler.setLevel(logging.ERROR)
+        #     app.logger.addHandler(mail_handler)
+
+        if not os.path.exists('/home/ubuntu/examsys/logs'):
+            os.mkdir('/home/ubuntu/examsys/logs')
+        file_handler = RotatingFileHandler('/home/ubuntu/examsys/logs/examsys.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('examsys startup')
 
     return app
